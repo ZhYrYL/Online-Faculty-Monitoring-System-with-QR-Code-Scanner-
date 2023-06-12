61dede9771b5541bd29a2e50ccabcf2ec403c9e8
Zhyryll <zhyryllposadas@gmail.com>
2023-06-12 12:54:03 +0800

    Created adminlogin.php for mobile to tablet and pc

---
+++ adminlogin.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Login Page</title>
+</head>
+<body>
+    <div class="adminheader">
+        <div class="adbanner">
+            <img src="img/psu-banner.png" alt="psu banner" class="psu-banner" id="psu-banner">
+        </div>
+    </div>
+    <div class="adminbody">
+        <div class="admbody">
+            <div class="adminlogin">
+                <div class="adlogheader">
+                    <h2>Admin Login Page</h2>
+                </div>
+                <div class="adinput1">
+                    <div class="username-icon">
+                        <div class="user-icon">
+                            <img src="img/user.png" alt="Username Icon">
+                        </div>
+                        <div class="user-input">
+                            <input type="text" name="username" placeholder="Username">
+                        </div>
+                    </div>
+                </div>
+                <div class="adinput2">
+                    <div class="password-icon">
+                        <div class="pass-icon">
+                            <img src="img/black-eye.png" alt="Username Icon">
+                        </div>
+                        <div class="pass-input">
+                            <input type="password" name="username" placeholder="Password">
+                        </div>
+                    </div>
+                </div>
+                <div class="adinput3">
+                    <input type="submit" name="adminlogin"  value="Login" class="admlogin">
+                </div>
+            </div>
+        </div>
+        <div class="admfooter">2</div>
+    </div>
+</body>
+</html>
