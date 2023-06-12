86f00cf0e24930551f4e209b9165e36e56107394
Zhyryll <zhyryllposadas@gmail.com>
2023-06-12 11:23:18 +0800

    Created account.php layout for mobile to tablet and pc

---
+++ account.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="hhead">
+        <div class="profile">
+            <a href="homepage.php"><img src="img/back.png" alt="back button" class="back-btn"></a>
+        </div>
+        <div class="header">
+            <h2>Account Name</h2>
+        </div>
+    </div>
+    <div class="abody">
+        <div class="abody1">
+            <div class="acc-pic">
+                <img src="img/posadas.jpg" alt="instructor picture" class="instruc-pic">
+            </div>
+            <div class="afaculty">
+                <h2>Faculty</h2>
+            </div>
+        </div>
+        <div class="abody2">
+            <div class="aemail">
+                <div class="aemail1">
+                    <h3>Email</h3>
+                </div>
+                <div class="aemail2">
+                    <p>instructor@gmail.com</p>
+                </div>
+            </div>
+            <div class="aphone">
+                <div class="aphone1">
+                    <h3>Mobile Phone</h3>
+                </div>
+                <div class="aphone2">
+                    <p>09123456789</p>
+                </div>
+            </div>
+            <div class="aempty">3</div>
+        </div>
+    </div>
+</body>
+</html>
